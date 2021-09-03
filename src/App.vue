634f8a39d16ad064f771abecb4b2b988c475a25b
Felix Ranesberger <felix.ranesberger@in2code.de>
2021-09-03 10:37:09 +0200

    [FEATURE] First sorking version of color naming tool

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,63 @@
 <template>
-  <div class="center">
-    <input v-model="color" placeholder="edit me">
+  <div
+    class="center"
+    :class="!isColorPickerHidden ? 'isActive' : false"
+    @click.self="deactivateColorPicker"
+  >
+    <ColorPicker
+      theme="dark"
+      :color="color"
+      :colors-default="colorPickerDefaultColors"
+      v-show="!isColorPickerHidden"
+      class="colorpicker"
+      @changeColor="updateColor"
+    />
 
     <h1>Hi! I'm
-      <pre>{{ name }}</pre>
+      <pre @click="isColorPickerHidden = !isColorPickerHidden">{{ name }}</pre>
+      <img
+        v-show="isValidColor"
+        src="src/assets/emoji_worked.png"
+        alt="memoji happy"
+      />
+      <img
+        v-show="!isValidColor"
+        src="src/assets/emoji_not_working.png"
+        alt="memoji sad"
+      />
     </h1>
   </div>
 </template>
 
 <script>
+import { ColorPicker } from 'vue-color-kit';
+import 'vue-color-kit/dist/vue-color-kit.css';
 
 export default {
   name: 'App',
+  components: {
+    ColorPicker,
+  },
+  computed: {
+    colorPickerDefaultColors() {
+      return [
+        '#A5C85A',
+        '#32DCE6',
+        '#D24632',
+        '#FAF046',
+        '#FF8000',
+        '#858C8C',
+        '#0A1919',
+        '#2862E9',
+      ];
+    },
+  },
   data() {
     return {
+      color: '#8db33c',
       name: '',
-      color: '123123',
+      isValidColor: false,
+      isColorPickerHidden: true,
     };
   },
   watch: {
@@ -25,19 +67,34 @@ export default {
   },
   methods: {
     async fetchColorName() {
-      const strippedColor = this.color.replace('#', '');
-      const data = await fetch(`https://api.color.pizza/v1/${strippedColor}`)
-        .then((response) => (response.status === 200 ? response.json() : false));
+      console.log(1630657709719, this.isColorPickerHidden);
+      if (this.isColorPickerHidden) {
+        const strippedColor = this.color.replace('#', '');
+        const data = await fetch(`https://api.color.pizza/v1/${strippedColor}`)
+          .then((response) => (response.status === 200 ? response.json() : false));
 
-      console.log(1630601571129, data);
+        console.log(1630601571129, data);
 
-      const name = data
-        ? data.colors[0].name
-        : 'not a valid color 😇';
-
-      console.log(1630601552991, name);
-
-      this.name = name;
+        if (data && this.color.length > 0) {
+          this.name = data.colors[0].name;
+          this.isValidColor = true;
+        } else {
+          this.name = 'not a valid color';
+          this.isValidColor = false;
+        }
+      }
+    },
+    updateColor(color) {
+      console.log(1630654717546, 'test');
+      console.log(1630654680509, color);
+      this.color = color.hex;
+    },
+    deactivateColorPicker() {
+      console.log(1630656948611, 'true');
+      if (!this.isColorPickerHidden) {
+        this.isColorPickerHidden = true;
+        this.fetchColorName();
+      }
     },
   },
   mounted() {
@@ -46,7 +103,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 
 html,
 body {
@@ -61,6 +118,24 @@ body {
 
 #app, .center {
   height: 100%;
+}
+
+.isActive {
+  &:after {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(black, 60%);
+    content: '';
+    z-index: 50;
+  }
+}
+
+.colorpicker {
+  position: absolute;
+  z-index: 100;
 }
 
 input {
@@ -87,13 +162,26 @@ input {
 }
 
 h1 {
+  display: flex;
   font-weight: bold;
   font-size: 70px;
 }
 
 pre {
+  margin: 0 0 0 1.5rem;
   display: inline-block;
+  position: relative;
   padding: 5px 25px;
   background-color: #FEDF00;
+
+  &:hover {
+    cursor: pointer;
+   }
+}
+
+img {
+  margin-left: .4rem;
+  height: 96px;
+  width: auto;
 }
 </style>
